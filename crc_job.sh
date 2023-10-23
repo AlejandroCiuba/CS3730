7c27e9517a2f87e4d236e318ebf1f8b5de7266dd
Alejandro Ciuba <alejandrociuba@gmail.com>
2023-10-23 01:10:56 -0400

    Changed time to 5 days

--- crc_job.sh
+++ crc_job.sh
@@ -14,7 +14,7 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=4-00:00:00
+#SBATCH --time=5-00:00:00
 #SBATCH --qos=short
 ############## SBATCH HEADER END ##############
 
