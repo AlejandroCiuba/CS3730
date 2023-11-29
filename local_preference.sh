628f55a8a1320ffced9c4d64667564b04cb6708e
Alejandro <alejandrociuba@gmail.com>
2023-11-29 04:07:12 -0500

    Fixed another small error in local_preference.sh

--- local_preference.sh
+++ local_preference.sh
@@ -8,26 +8,26 @@ version=`python finetune.py --version`
 
 echo "RUNNING $version SCRIPT"
 
-python finetune.py -m google/mt5-small \
-				   -d datasets/opus_test \
-				   -s train \
-				   -sl en \
-				   -lc 1 \
-				   -tl es \
-                   -ts 0.3 \
-                   -op 0 \
-				   -tb 128 \
-				   -t "English to Spanish" \
-				   -me sacrebleu \
-				   -mk score \
-				   -sk 1 \
-				   -f 1 \
-				   -l 4e-5 \
-				   -e 1 \
-				   -b 2 \
-				   -sa 0.5 \
-				   -x 5 \
-				   -o models/test_pref1 \
-                   -lo logs
+python finetune_preference.py -m google/mt5-small \
+							  -d datasets/opus_test \
+							  -s train \
+							  -sl en \
+							  -lc 1 \
+							  -tl es \
+							  -ts 0.3 \
+							  -op 0 \
+							  -tb 128 \
+							  -t "English to Spanish" \
+							  -me sacrebleu \
+							  -mk score \
+							  -sk 1 \
+							  -f 1 \
+							  -l 4e-5 \
+							  -e 1 \
+							  -b 2 \
+							  -sa 0.5 \
+							  -x 5 \
+							  -o models/test_pref1 \
+							  -lo logs
 
 echo "DONE"
